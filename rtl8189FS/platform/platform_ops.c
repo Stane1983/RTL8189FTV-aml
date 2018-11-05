11cdc07dd29ab4055ec76974e246708ae8457605
Rongjun Chen <rongjun.chen@amlogic.com>
2018-11-05 14:06:07 +0800

    wifi: update rtl8189ftv driver to v5.3.12_28613.20180703
    Change-Id: I668b96837381cbee2437baf431a421182491a80b

--- rtl8189FS/platform/platform_ops.c
+++ rtl8189FS/platform/platform_ops.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright(c) 2013 Realtek Corporation. All rights reserved.
+ * Copyright(c) 2013 - 2017 Realtek Corporation.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -11,17 +11,8 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
- *
- *
- ******************************************************************************/
-#include <linux/time.h>
-#include <linux/delay.h>
+ *****************************************************************************/
 #ifndef CONFIG_PLATFORM_OPS
-extern void sdio_reinit(void);
-extern void extern_wifi_set_enable(int is_on);
 /*
  * Return:
  *	0:	power on successfully
@@ -31,16 +22,11 @@ int platform_wifi_power_on(void)
 {
 	int ret = 0;
 
-	printk("######%s: \n",__func__);
-	extern_wifi_set_enable(0);
-	msleep(500);
-	extern_wifi_set_enable(1);
-	msleep(500);
-	sdio_reinit();
+
 	return ret;
 }
 
 void platform_wifi_power_off(void)
 {
 }
-#endif // !CONFIG_PLATFORM_OPS
+#endif /* !CONFIG_PLATFORM_OPS */
